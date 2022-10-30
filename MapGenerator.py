9371e9622a9dab8a01b870ffb8f49e508ea05f7e
vincowl <vincent@heliosphere.fr>
2022-10-30 23:31:57 +0100

    Resolves feat 1 - 2

--- MapGenerator.py
+++ MapGenerator.py
@@ -40,6 +40,28 @@ class MapGenerator:
         return
        
     def isMarkerFormateur(self, isFormateur):
+#          Parameters
+#     ----------
+#     color : str, default 'blue'
+#         The color of the marker. You can use:
+#             ['red', 'blue', 'green', 'purple', 'orange', 'darkred',
+#              'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue',
+#              'darkpurple', 'white', 'pink', 'lightblue', 'lightgreen',
+#              'gray', 'black', 'lightgray']
+#     icon_color : str, default 'white'
+#         The color of the drawing on the marker. You can use colors above,
+#         or an html color code.
+#     icon : str, default 'info-sign'
+#         The name of the marker sign.
+#         See Font-Awesome website to choose yours.
+#         Warning : depending on the icon you choose you may need to adapt
+#         the `prefix` as well.
+#     angle : int, default 0
+#         The icon will be rotated by this amount of degrees.
+#     prefix : str, default 'glyphicon'
+#         The prefix states the source of the icon. 'fa' for font-awesome or
+#         'glyphicon' for bootstrapp3.
+        #return folium.Icon(color='blue', icon='leaf', icon_color='white')
         try:
             float(isFormateur)
             return None
@@ -55,18 +77,37 @@ class MapGenerator:
         attr = 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         tiles = 'https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}'
 
-        myMap = folium.Map(location=SF_COORDINATES, tiles=tiles, attr=attr, zoom_start=2)
+        myMap = folium.Map(location=SF_COORDINATES, tiles=None, zoom_start=2)
+        myMap.add_tile_layer(tiles='OpenStreetMap',  name='OpenStreetMap')
+        myMap.add_tile_layer(tiles=tiles, attr=attr, name='Heidelberg Uni')
         # myMap = folium.Map(location=SF_COORDINATES, zoom_start=2)
-        marker_cluster = MarkerCluster().add_to(myMap)
+        marker_cluster_parents = MarkerCluster(name='Parents',
+                                       overlay=True,
+                                       control=True,
+                                       icon_create_function=None)
+        marker_cluster_parents.add_to(myMap)
+        marker_cluster_formateurs = MarkerCluster(name='Formateurs',
+                                       overlay=True,
+                                       control=True,
+                                       icon_create_function=None)
+        marker_cluster_formateurs.add_to(myMap)
+        
         # add a marker for every record in the filtered data, use a clustered view
         for each in self._mergedData[0:MAX_RECORDS].iterrows():
             chaine = "<strong>"+each[1]['Nom']+"</strong><br/>"+each[1]['nom_commune']+'<br/><a href="https://www.facebook.com/messages/t/'+each[1]['identifiant FB']+'">Contacter</a>'
-            folium.Marker(
+            icone = self.isMarkerFormateur(each[1]['Formateur'])
+            marker = folium.Marker(
                 location=[float(each[1]['latitude']), float(each[1]['longitude'])],
                 popup=chaine,
-                icon=self.isMarkerFormateur(each[1]['Formateur']),
-                ).add_to(marker_cluster)
+                icon=icone,
+                )
+            if icone:
+                marker.add_to(marker_cluster_formateurs)
+            else:
+                marker.add_to(marker_cluster_parents)
+        folium.LayerControl().add_to(myMap)
         myMap.save('liste_coco.html') #save HTML
         #display(myMap)
         return
+
 
